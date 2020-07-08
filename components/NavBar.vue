50e34cfc9309a211eb69128c85b7b0c9fee53348
vproskurin <vladimir.proskurin@gmail.com>
2020-07-07 21:27:27 -0700

    routing

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,7 +3,7 @@
   <nuxt-link to="/" class="brand">To Events</nuxt-link>
   <nav>
     <nuxt-link to="/">List</nuxt-link> |
-    <nuxt-link to="/create">Create</nuxt-link>
+    <nuxt-link to="/event/create">Create</nuxt-link>
   </nav>
 </div>
 </template>
