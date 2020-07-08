dd047ff9ded51cf1939de1ae8ff9ea0756f237cb
vproskurin <vladimir.proskurin@gmail.com>
2020-07-07 21:27:57 -0700

    all files from routing

---
+++ pages/event/_id.vue
@@ -0,0 +1,29 @@
+<template> 
+<div>
+  <h1>Event # {{ id }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Event #" + this.id,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "what you need to know about event #" + this.id
+        }
+      ]
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+};
+</script>
+
+<style></style>
